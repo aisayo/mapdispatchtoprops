319c244db9d36e6a5979bc4cfd32165241347f76
Aysan Isayo <aisayo@icloud.com>
2020-09-16 08:59:00 -0700

    Created rootReducer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import rootReducer from './rootReducer'
+import rootReducer from './reducers/rootReducer'
 
 const store = createStore(
         rootReducer, 
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    students: []
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'ADD_STUDENT':
+            return {...state, students: [...state.students, action.payload]}
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
